0d4a6dce4dab1ce74f3f777926c9f84ddeff9b4d
eugene_prikhach <eugeneprikhach@icloud.com>
2020-11-19 20:07:49 +0300

    Optimization of the program

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sklearn.cluster import KMeans
-from scipy.cluster.hierarchy import dendrogram, ward, complete, average
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as sch
 from csv import DictReader
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,77 +12,30 @@ def fill_cluster(first, second, third):
     varieties.clear()
     with open('users_info.csv', 'r') as read_obj:
         csv_dict_reader = DictReader(read_obj)
-        if third == '':
-            for row in csv_dict_reader:
-                if row['Age'] != '' and row['City'] != '' and row['City_id'] != '':
-                    varieties.append([row[first], row[second]])
-        else:
-            for row in csv_dict_reader:
-                if row['Age'] != '' and row['City'] != '' and row['City_id'] != '':
-                    varieties.append([row[first], row[second], row[third]])
-
-def k_mean():
-    cluster_data = np.array(varieties)
-    plt.scatter(cluster_data[:, 0], cluster_data[:, 1], label='Some label')
-    plt.show()
-    kmeans = KMeans(n_clusters=7)
-    kmeans.fit(cluster_data)
-    print('Cluster centers: ')
-    print(kmeans.cluster_centers_)
-    plt.scatter(cluster_data[:, 0], cluster_data[:, 1], c=kmeans.labels_, cmap='rainbow')
-    plt.show()
+        try:
+            if third == '':
+                for row in csv_dict_reader:
+                    if row['Age'] != '' and row['City'] != '' and row['City_id'] != '':
+                        varieties.append([int(row[first]), int(row[second])])
+            else:
+                for row in csv_dict_reader:
+                    if row['Age'] != '' and row['City'] != '' and row['City_id'] != '':
+                        varieties.append([int(row[first]), int(row[second]), int(row[third])])
+        except:
+            pass
 
 
-def get_clusters_Ward():
-    cluster_data = np.array(varieties)
-    linkage_array = ward(cluster_data)
-    dendrogram(linkage_array)
-    ax = plt.gca()
-    bounds = ax.get_xbound()
-    ax.plot(bounds, [10000, 10000], '--', c='k')  # 7.25 to sample
-    ax.plot(bounds, [1100, 1100], '--', c='k')  # 4 to sample
-    ax.text(bounds[1], 10000, ' 2 Clusters', va='center', fontdict={'size': 5})
-    ax.text(bounds[1], 1100, ' 3 Clusters', va='center', fontdict={'size': 5})
-    plt.xlabel("Observation index")
-    plt.ylabel("Cluster Distance")
-    plt.show()
-
-
-def get_clusters_complete():
-    cluster_data = np.array(varieties)
-    linkage_array = complete(cluster_data)
-    dendrogram(linkage_array)
-    ax = plt.gca()
-    bounds = ax.get_xbound()
-    ax.plot(bounds, [10000, 10000], '--', c='k')  # 7.25 to sample
-    ax.plot(bounds, [1100, 1100], '--', c='k')  # 4 to sample
-    ax.text(bounds[1], 10000, ' 2 Clusters', va='center', fontdict={'size': 5})
-    ax.text(bounds[1], 1100, ' 3 Clusters', va='center', fontdict={'size': 5})
-    plt.xlabel("Observation index")
-    plt.ylabel("Cluster Distance")
-    plt.show()
-
-
-def get_clusters_average():
-    cluster_data = np.array(varieties)
-    linkage_array = average(cluster_data)
-    dendrogram(linkage_array)
-    ax = plt.gca()
-    bounds = ax.get_xbound()
-    ax.plot(bounds, [10000, 10000], '--', c='k')  # 7.25 to sample
-    ax.plot(bounds, [1100, 1100], '--', c='k')  # 4 to sample
-    ax.text(bounds[1], 10000, ' 2 Clusters', va='center', fontdict={'size': 5})
-    ax.text(bounds[1], 1100, ' 3 Clusters', va='center', fontdict={'size': 5})
-    plt.xlabel("Observation index")
-    plt.ylabel("Cluster Distance")
+def get_cluster(cluster_method):
+    x = np.array(varieties)
+    dendrogram = sch.dendrogram(sch.linkage(x, method=cluster_method))
     plt.show()
 
 
 def main():
     fill_cluster('Photos', 'Age', '')
-    get_clusters_complete()
-    fill_cluster('Age', 'City_id', 'Photos')
-    get_clusters_Ward()
+    get_cluster('ward')
+    fill_cluster('Age', 'City_id', 'Profile_entries')
+    get_cluster('average')
 
 
 if __name__ == "__main__":
